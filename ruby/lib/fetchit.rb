f025c8cb86ca64015202fff73b5a729dcf02e7fb
Colin Seymour <colin@github.com>
2014-08-25 16:04:56 +0100

    Added support for options and optimized things a little

--- ruby/lib/fetchit.rb
+++ ruby/lib/fetchit.rb
@@ -9,7 +9,6 @@ require "base64"
 require "time"
 require "yaml"
 
-
 CONFIG = YAML.load_file("config.yml") unless defined? CONFIG
 
 class FetchEveryone
@@ -25,10 +24,11 @@ class FetchEveryone
     @access_token = response['access_token']
   end
 
-  def getResource(resource)
+  def getResource(resource, options = nil)
+    encoded_opts = "&#{URI.encode_www_form(options)}" unless options.nil?
     response = self.class.get(
-      "https://api.fetcheveryone.com/api.php?request=#{resource}",
-      headers: {'Authorization' => "Bearer #{@access_token}", 'Content-type' => 'application/json'}
+      "https://api.fetcheveryone.com/api.php?request=#{resource}#{encoded_opts}",
+      headers: {'Authorization' => "Bearer #{@access_token}", 'Content-type' => 'application/json'},
     )
     puts response.body
   end
@@ -36,4 +36,5 @@ class FetchEveryone
 end
 
 fe = FetchEveryone.new("#{CONFIG['api_key']}", "#{CONFIG['api_secret']}")
-fe.getResource("forum/threads")
+#fe.getResource("forum/threads")
+fe.getResource("forum/threads", {category: "training", items: 5})
